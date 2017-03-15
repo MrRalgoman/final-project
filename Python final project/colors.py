c6123b6141a91796de6fdc0b02a9e7c4c4c5fe2c
Luke Ralgoman <Luke Ralgoman>
2017-03-14 23:35:00 -0700

    sorting out files

--- Python final project/colors.py
+++
@@ -1,9 +0,0 @@
-# Add colors here not in main script
-
-white = (255,255,255)
-black = (  0,  0,  0)
-grey  = ( 40, 40, 40)
-red   = (255,  0,  0)
-green = (  0,255,  0)
-blue  = (  0,  0,255)
-light = (200,200,200)
